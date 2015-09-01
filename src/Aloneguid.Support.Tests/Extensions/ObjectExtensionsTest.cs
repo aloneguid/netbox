4a8fab4d71082e7d950f13fbbdac92921b380c92
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2015-09-01 19:09:13 +0100

    all StringExtensions now have portable tests as well

---
+++ src/Aloneguid.Support.Tests/Extensions/ObjectExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿extern alias Portable;
+using System;
+using NUnit.Framework;
+
+namespace Aloneguid.Support.Tests.Extensions
+{
+   [TestFixture]
+   public class ObjectExtensionsTest
+   {
+      [Test]
+      public void XmlSerialise_Null_Null()
+      {
+         string s = ((object)null).XmlSerialise();
+         Assert.IsNull(s);
+      }
+
+      [Test]
+      public void PortableXmlSerialise_Null_Null()
+      {
+         string s = Portable::System.ObjectExtensions.XmlSerialise((object)null);
+         Assert.IsNull(s);
+      }
+   }
+}
--- src/Aloneguid.Support.Tests/Extensions/StringExtensionsTest.cs
+++ src/Aloneguid.Support.Tests/Extensions/StringExtensionsTest.cs
@@ -8,20 +8,6 @@ namespace Aloneguid.Support.Tests.Extensions
    public class StringExtensionsTest
    {
       [Test]
-      public void XmlSerialise_Null_Null()
-      {
-         string s = ((object)null).XmlSerialise();
-         Assert.IsNull(s);
-      }
-
-      [Test]
-      public void PortableXmlSerialise_Null_Null()
-      {
-         string s = Portable::System.ObjectExtensions.XmlSerialise((object) null);
-         Assert.IsNull(s);
-      }
-
-      [Test]
       public void XmlDeserialise_Null_Null()
       {
          XmlDoc doc = ((string)null).XmlDeserialise<XmlDoc>();
@@ -51,6 +37,24 @@ namespace Aloneguid.Support.Tests.Extensions
          Assert.AreEqual("test", d3.SV);
       }
 
+
+      [Test]
+      public void PortableXmlSerialiseDeserialise_Object_ValidString()
+      {
+         XmlDoc d1 = new XmlDoc
+         {
+            SV = "test"
+         };
+
+         string s = Portable::System.ObjectExtensions.XmlSerialise(d1);
+         XmlDoc d2 = Portable::System.StringExtensions.XmlDeserialise<XmlDoc>(s);
+         XmlDoc d3 = (XmlDoc)Portable::System.StringExtensions.XmlDeserialise(s, typeof(XmlDoc));
+
+         Assert.AreEqual("test", d2.SV);
+         Assert.AreEqual("test", d3.SV);
+      }
+
+
       [Test]
       [ExpectedException(typeof(InvalidOperationException))]
       public void XmlSerialise_HiddenObject_Fails()
@@ -61,10 +65,26 @@ namespace Aloneguid.Support.Tests.Extensions
 
       [Test]
       [ExpectedException(typeof(InvalidOperationException))]
+      public void PortableXmlSerialise_HiddenObject_Fails()
+      {
+         var ho = new HiddenDoc();
+         Portable::System.ObjectExtensions.XmlSerialise(ho);
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
       public void XmlSerialise_NonXmlObject_Fails()
       {
          var nxo = new NonXmlDoc(5);
          nxo.XmlSerialise();
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void PortableXmlSerialise_NonXmlObject_Fails()
+      {
+         var nxo = new NonXmlDoc(5);
+         Portable::System.ObjectExtensions.XmlSerialise(nxo);
       }
 
       [Test]
@@ -84,11 +104,38 @@ namespace Aloneguid.Support.Tests.Extensions
       }
 
       [Test]
+      public void PortableJsonSerialise_Object_ValidString()
+      {
+         var d1 = new XmlDoc
+         {
+            SV = "test"
+         };
+
+         string s = Portable::System.ObjectExtensions.ToJsonString(d1);
+         Assert.IsNotNull(s);
+
+         XmlDoc d2 = Portable::System.StringExtensions.AsJsonObject<XmlDoc>(s);
+
+         Assert.AreEqual(d1.SV, d2.SV);
+      }
+
+
+      [Test]
       public void Base64_Encode_Decodes()
       {
          string s = "test string";
          string s64 = s.Base64Encode();
          string s2 = s64.Base64Decode();
+
+         Assert.AreEqual(s, s2);
+      }
+
+      [Test]
+      public void PortableBase64_Encode_Decodes()
+      {
+         string s = "test string";
+         string s64 = Portable::System.StringExtensions.Base64Encode(s);
+         string s2 = Portable::System.StringExtensions.Base64Decode(s64);
 
          Assert.AreEqual(s, s2);
       }
