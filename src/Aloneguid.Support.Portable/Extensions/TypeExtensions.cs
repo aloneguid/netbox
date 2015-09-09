cfd5cf330cef11178d3d1a6c2b662ffcf5034f01
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2015-09-09 11:37:38 +0100

    comment on IsEnum

--- src/Aloneguid.Support.Portable/Extensions/TypeExtensions.cs
+++ src/Aloneguid.Support.Portable/Extensions/TypeExtensions.cs
@@ -3,10 +3,16 @@ namespace System
 {
    public static class TypeExtensions
    {
+      /// <summary>
+      /// More reliable way to determine if a type is Enum, especially in a portable reflection
+      /// </summary>
       public static bool IsEnum(this Type t)
       {
+         if(t == null) return false;
+
          try
          {
+            // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
             Enum.GetUnderlyingType(t);
             return true;
          }
