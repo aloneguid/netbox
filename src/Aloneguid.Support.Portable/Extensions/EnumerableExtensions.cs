5d8f92827f2f1eba84b2440da88cb817f6bc183c
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2015-09-10 14:40:22 +0100

    Chunk() documentation and tests

--- src/Aloneguid.Support.Portable/Extensions/EnumerableExtensions.cs
+++ src/Aloneguid.Support.Portable/Extensions/EnumerableExtensions.cs
@@ -6,21 +6,28 @@ namespace System.Collections.Generics
 {
    public static class EnumerableExtensions
    {
-      public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize)
+      /// <summary>
+      /// Split sequence in batches of specified size
+      /// </summary>
+      /// <typeparam name="T">Element type</typeparam>
+      /// <param name="source">Enumeration source</param>
+      /// <param name="chunkSize">Size of the batch chunk</param>
+      /// <returns></returns>
+      public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunkSize)
       {
-         if(source == null) throw new ArgumentNullException("source");
+         if(source == null) throw new ArgumentNullException(nameof(source));
 
          while(source.Any())
          {
-            yield return source.Take(chunksize);
-            source = source.Skip(chunksize);
+            yield return source.Take(chunkSize);
+            source = source.Skip(chunkSize);
          }
       }
 
       public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
       {
-         if(source == null) throw new ArgumentNullException("source");
-         if(action == null) throw new ArgumentNullException("action");
+         if(source == null) throw new ArgumentNullException(nameof(source));
+         if(action == null) throw new ArgumentNullException(nameof(action));
 
          foreach(T element in source)
          {
---
+++ src/Aloneguid.Support.Tests/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Aloneguid.Support.Tests.Extensions
+{
+   [TestFixture]
+   public class EnumerableExtensionsTest
+   {
+      [Test]
+      public void Chunk_LongSequence_Splits()
+      {
+         string[] sequence = {"one", "two", "three", "four", "five"};
+
+         List<IEnumerable<string>> chunks = sequence.Chunk(2).ToList();
+
+         Assert.AreEqual(3, chunks.Count);
+         Assert.AreEqual(2, chunks[0].Count());
+         Assert.AreEqual(2, chunks[1].Count());
+         Assert.AreEqual(1, chunks[2].Count());
+      }
+   }
+}
