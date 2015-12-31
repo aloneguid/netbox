f5f7ae47487de08d3b93228656d6b048a87b1e46
Ivan Gavryliuk <ivan.gavrilyuk@gmail.com>
2015-12-31 11:39:15 +0000

    added datagenerator

---
+++ src/Aloneguid.Support.Portable/Generator.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.IO;
+using System.Text;
+#if !PORTABLE
+using System.Collections.Generic;
+using System.Reflection;
+#endif
+
+namespace Aloneguid.Support
+{
+   public static class Generator
+   {
+      private static readonly Random Rnd = new Random(DateTime.UtcNow.Millisecond);
+
+      public static bool RandomBool
+      {
+         get { return Rnd.Next(2) == 1; }
+      }
+
+      public static long RandomLong
+      {
+         get { return GetRandomLong(0, long.MaxValue); }
+      }
+
+      public static int RandomInt
+      {
+         get { return Rnd.Next(); }
+      }
+
+      public static int GetRandomInt(int max)
+      {
+         return Rnd.Next(max);
+      }
+
+      public static int GetRandomInt(int min, int max)
+      {
+         return Rnd.Next(min, max);
+      }
+
+      public static long GetRandomLong(long min, long max)
+      {
+         long randomLong = min + (long)(Rnd.NextDouble() * (max - min));
+         return randomLong;
+      }
+
+      public static Enum RandomEnum(Type t)
+      {
+         Array values = Enum.GetValues(t);
+
+         object value = values.GetValue(Rnd.Next(values.Length));
+
+         return (Enum)value;
+      }
+
+#if !PORTABLE
+      public static T RandomEnum<T>() where T : struct
+      {
+         //can't limit generics to enum http://connect.microsoft.com/VisualStudio/feedback/details/386194/allow-enum-as-generic-constraint-in-c
+
+         if(!typeof(T).IsEnum) throw new ArgumentException("T must be an enum");
+
+         return (T)(object)RandomEnum(typeof(T));
+      }
+#endif
+
+      public static DateTime GetRandomDate(DateTime minValue, DateTime maxValue)
+      {
+         long randomTicks = GetRandomLong(minValue.Ticks, maxValue.Ticks);
+
+         return new DateTime(randomTicks);
+      }
+
+      public static DateTime RandomDate
+      {
+         get { return GetRandomDate(DateTime.MinValue, DateTime.MaxValue); }
+      }
+
+      public static string RandomString
+      {
+         get
+         {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", "");
+            return path;
+         }
+      }
+
+      public static string GetRandomString(int maxLength, bool allowNulls)
+      {
+         if(allowNulls && RandomLong % 2 == 0) return null;
+
+         var builder = new StringBuilder();
+         char ch;
+         for(int i = 0; i < maxLength; i++)
+         {
+            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * Rnd.NextDouble() + 65)));
+            builder.Append(ch);
+         }
+
+         return builder.ToString();
+      }
+
+      public static Uri GetRandomUri(bool allowNulls)
+      {
+         if(allowNulls && RandomLong % 2 == 0) return null;
+
+         return new Uri(string.Format("http://{0}.com/{1}.{2}", RandomString, RandomString, GetRandomString(3, false)));
+      }
+
+      public static Uri RandomUri
+      {
+         get { return GetRandomUri(false); }
+      }
+
+#if !PORTABLE
+
+      public static T RandomClass<T>() where T : class, new()
+      {
+         T result = new T();
+
+         foreach(PropertyInfo property in result.GetType().GetProperties())
+         {
+            if(property.CanWrite)
+            {
+               Type t = property.PropertyType;
+               object setValue;
+
+               if(t == typeof(string)) setValue = RandomString;
+               else if(t == typeof(long)) setValue = RandomLong;
+               else if(t == typeof(int)) setValue = RandomInt;
+               else if(t == typeof(bool)) setValue = RandomBool;
+               else if(t == typeof(Guid)) setValue = Guid.NewGuid();
+               else if(t == typeof(DateTime)) setValue = RandomDate;
+               else if(t.IsEnum) setValue = RandomEnum(t);
+               else
+               {
+                  throw new InvalidOperationException(string.Format("type {0} cannot be generated on property {1}", t,
+                     property.Name));
+               }
+
+               property.SetValue(result, setValue, null);
+            }
+         }
+
+         return result;
+      }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <typeparam name="T"></typeparam>
+      /// <param name="count"></param>
+      /// <param name="allowNulls">When set to true some of the returned elements may be null</param>
+      /// <returns></returns>
+      public static IEnumerable<T> RandomClassCollection<T>(int count, bool allowNulls) where T : class, new()
+      {
+         var result = new List<T>();
+         for(int i = 0; i < count; i++)
+         {
+            bool isNull;
+            if(!allowNulls) isNull = false;
+            else isNull = RandomLong % 2 == 0;
+
+            if(isNull)
+            {
+               result.Add(null);
+            }
+            else
+            {
+               result.Add(RandomClass<T>());
+            }
+         }
+         return result;
+      }
+
+#endif
+
+   }
+}
---
+++ src/Aloneguid.Support.Tests/GeneratorTests.cs
@@ -0,0 +1,164 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aloneguid.Support.Tests
+{
+   [TestFixture]
+   public class GeneratorTest
+   {
+      [Test]
+      public void RandomString_TwoStrings_NotEqual()
+      {
+         string s1 = Generator.RandomString;
+         string s2 = Generator.RandomString;
+
+         Assert.AreNotEqual(s1, s2);
+      }
+
+      [TestCase(10)]
+      [TestCase(100)]
+      public void RandomString_SpecificLengthNoNulls_Matches(int length)
+      {
+         Assert.AreEqual(length, Generator.GetRandomString(length, false).Length);
+      }
+
+      [Test]
+      public void RandomClass_TwoClasses_NotEqual()
+      {
+         RandomClassExample rc1 = Generator.RandomClass<RandomClassExample>();
+         RandomClassExample rc2 = Generator.RandomClass<RandomClassExample>();
+
+         Assert.IsNotNull(rc1);
+         Assert.IsNotNull(rc2);
+         Assert.IsNotNull(rc1.StringProperty);
+         Assert.AreNotEqual(0, rc1.LongProperty);
+         Assert.AreNotEqual(rc1.LongProperty, rc2.LongProperty);
+         Assert.AreNotEqual(rc1.StringProperty, rc2.StringProperty);
+      }
+
+      [TestCase(1000, true)]
+      [TestCase(1000, false)]
+      public void RandomClassCollection_ManyElements_DoesNotFail(int count, bool allowNulls)
+      {
+         List<RandomClassExample> collection = Generator.RandomClassCollection<RandomClassExample>(
+            count,
+            allowNulls).ToList();
+
+         Assert.AreEqual(count, collection.Count);
+
+         bool hasNulls = collection.Any(e => e == null);
+
+         if(allowNulls)
+         {
+            Assert.IsTrue(hasNulls, "doesn't have any nulls");
+         }
+         else
+         {
+            Assert.IsFalse(hasNulls, "must not have nulls");
+         }
+      }
+
+      [Test]
+      public void RandomBool_Anything_DoesntCrash()
+      {
+         bool b = Generator.RandomBool;
+      }
+
+      [Test]
+      public void RandomEnum_Random_Random()
+      {
+         EnumExample random = Generator.RandomEnum<EnumExample>();
+
+         //not sure how to validate
+      }
+
+      [Test]
+      public void RandomEnumNonGeneric_Random_Random()
+      {
+         EnumExample random = (EnumExample)Generator.RandomEnum(typeof(EnumExample));
+
+         //not sure how to validate
+      }
+
+      [Test]
+      public void RandomInt_Random_Random()
+      {
+         int i = Generator.RandomInt;
+      }
+
+      // ReSharper disable NUnit.MethodWithParametersAndTestAttribute
+      [Test]
+      public void RandomInt_MaxValue_LessThan(
+         [Random(0, int.MaxValue, 100)]
+         int maxValue)
+      {
+         int i = Generator.GetRandomInt(maxValue);
+
+         Assert.Less(i, maxValue);
+      }
+      // ReSharper restore NUnit.MethodWithParametersAndTestAttribute
+
+      [Test]
+      public void RandomDate_Interval_Matches()
+      {
+         DateTime randomDate = Generator.GetRandomDate(DateTime.UtcNow, DateTime.UtcNow.AddDays(10));
+      }
+
+      [Test]
+      public void RandomDate_Random_Random()
+      {
+         DateTime randomDate = Generator.RandomDate;
+      }
+
+
+      [TestCase(-10L, 100L)]
+      [TestCase(5L, 10L)]
+      [TestCase(-100L, -1L)]
+      public void RandomLong_VaryingRange_InRange(long min, long max)
+      {
+         long random = Generator.GetRandomLong(min, max);
+
+         Assert.GreaterOrEqual(random, min);
+         Assert.LessOrEqual(random, max);
+      }
+
+      [Test]
+      public void RandomLong_TwoGenerations_NotEqual()
+      {
+         long l1 = Generator.RandomLong;
+         long l2 = Generator.RandomLong;
+
+         Assert.AreNotEqual(l1, l2);
+         Assert.Pass("{0} != {1}", l1, l2);
+      }
+
+      [Test]
+      public void RandomUri_TwoGenerations_NotEqual()
+      {
+         Uri u1 = Generator.GetRandomUri(false);
+         Uri u2 = Generator.RandomUri;
+
+         Assert.AreNotEqual(u1, u2);
+      }
+
+      private enum EnumExample
+      {
+         One,
+         Two,
+         Three
+      }
+
+      private class RandomClassExample
+      {
+         public string StringProperty { get; set; }
+
+         public long LongProperty { get; set; }
+
+         public bool BoolProperty { get; set; }
+
+         public EnumExample EnumProperty { get; set; }
+      }
+   }
+}
