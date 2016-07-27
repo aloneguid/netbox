5cb96e1011ef5fe454f2fe7b20cd1fbec925d642
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2016-07-27 15:12:46 +0100

    moving PasswordGenerator to core

---
+++ src/Aloneguid.Support.NetCore.Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aloneguid.Support.NetCore.Tests
+{
+   public class Program
+   {
+      public static void Main(string[] args)
+      {
+         Console.WriteLine(Generator.GetRandomInt(0, 10));
+         Console.ReadLine();
+      }
+   }
+}
