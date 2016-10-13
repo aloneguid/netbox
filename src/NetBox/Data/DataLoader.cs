78af54fdb3012e2a07d1bd35df3598b07bae2b85
Ivan Gavryliuk <ivan@isolineltd.com>
2016-10-13 12:05:00 +0100

    DataLoader now works in .net standard! yay!

--- src/NetBox/Data/DataLoader.cs
+++ src/NetBox/Data/DataLoader.cs
@@ -6,20 +6,12 @@ namespace NetBox.Data
    {
       public static string[] LoadFirstNames()
       {
-#if NETFULL
-         return Assembly.GetExecutingAssembly().GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("first-names.txt");
-#else
-         return Assembly.GetEntryAssembly().GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("first-names.txt");
-#endif
+         return G.ThisAssembly.GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("first-names.txt");
       }
 
       public static string[] LoadLastNames()
       {
-#if NETFULL
-         return Assembly.GetExecutingAssembly().GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("last-names.txt");
-#else
-         return Assembly.GetEntryAssembly().GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("last-names.txt");
-#endif
+         return G.ThisAssembly.GetSameFolderEmbeddedResourceFileAsLines<DataLoader>("last-names.txt");
       }
    }
 }
--- src/NetBox/G.cs
+++ src/NetBox/G.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection;
 using System.Text;
 
 namespace NetBox
@@ -17,5 +18,27 @@ namespace NetBox
       /// Global default culture
       /// </summary>
       public static readonly CultureInfo C = CultureInfo.InvariantCulture;
+
+      private static Assembly _thisAsm;
+
+      internal static Assembly ThisAssembly
+      {
+         get
+         {
+            if(_thisAsm == null)
+            {
+#if NETFULL
+               _thisAsm = Assembly.GetExecutingAssembly();
+#endif
+
+#if NETSTANDARD
+               _thisAsm = Assembly.Load(new AssemblyName("NetBox"));
+#endif
+            }
+
+            return _thisAsm;
+         }
+      }
+
    }
 }
