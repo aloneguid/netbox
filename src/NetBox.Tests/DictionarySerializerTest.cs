4728e1cfac52447566b75a117d282158c07b1e23
Ivan Gavryliuk <ivan@isolineltd.com>
2016-11-30 16:37:09 +0000

    silly experiments with reflection

---
+++ src/NetBox.Tests/DictionarySerializerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetBox.Application;
+using Xunit;
+
+namespace NetBox.Tests
+{
+   public class DictionarySerializerTest
+   {
+      [Fact]
+      public void OneLevel_Serialize_Deserializes()
+      {
+         var obj = new SimpleOneLevel() { StringProperty = "one" };
+
+         var result = new DictionarySerializer().Serialize(obj);
+         result = new DictionarySerializer().Serialize(obj);
+      }
+   }
+
+   #region [ Test Data ]
+
+   public class SimpleOneLevel
+   {
+      public string StringProperty { get; set; }
+   }
+
+   #endregion
+}
