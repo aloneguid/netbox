4b840fc71c373818e127d44e0f0880980d1d6866
Ivan Gavryliuk <ivan@isolineltd.com>
2017-01-04 21:25:44 +0000

    documentation update

--- src/docgen.ps1
+++ src/docgen.ps1
@@ -1,3 +1,12 @@
 # Ran manually to regenerate markdown documentation pages
+# modules are taken from https://github.com/aloneguid/powershell
 
-$xlmDocs = @("")
+Import-Module "C:\dev\powershell\Markdown"
+
+$xmlDocs = "$PSScriptRoot\NetBox\bin\Debug\net45\NetBox.xml"
+$out = "$PSScriptRoot\..\doc\reference.md"
+
+$xmlDocs
+$out
+
+Convert-XmlDocToMarkdown -XmlDocPath $xmlDocs -OutPath $out
