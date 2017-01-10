8a094357ff5713d950fbee6c7ad3d3aad52476a8
Ivan Gavryliuk <ivan@isolineltd.com>
2017-01-10 20:29:16 +0000

    add ungzip to netfile

--- src/NetBox/Extensions/NetFile.cs
+++ src/NetBox/Extensions/NetFile.cs
@@ -28,16 +28,34 @@
          }
       }
 
-      /*
       /// <summary>
       /// Ungzips the file
       /// </summary>
       /// <param name="filePath">Path to the file to ungzip</param>
-      /// <param name="tagetPath">Target file for the ungzipped file. Optional. When null source file must end with
+      /// <param name="targetPath">Target file for the ungzipped file. Optional. When null source file must end with
       /// .gz extension and target file will be decompressed to a file without it. For example mytextfile.txt.gz => mytextfile.txt</param>
-      public static void Ungzip(string filePath, string tagetPath)
+      public static void Ungzip(string filePath, string targetPath)
       {
+         if (filePath == null) throw new ArgumentNullException(nameof(filePath));
+         if (!File.Exists(filePath)) throw new ArgumentException("file does not exist", nameof(filePath));
 
-      }*/
+         if (targetPath == null)
+         {
+            if(!filePath.EndsWith(".gz", StringComparison.OrdinalIgnoreCase))
+            {
+               throw new ArgumentException("value is null and source file is not ending with .gz extension, cannot guess target filename", nameof(targetPath));
+            }
+
+            targetPath = filePath.Substring(0, filePath.Length - 3);
+         }
+
+         using (FileStream source = File.OpenRead(filePath))
+         {
+            using (Stream target = File.Create(targetPath))
+            {
+               source.Ungzip(target);
+            }
+         }
+      }
    }
 }
