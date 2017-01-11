540656f6c65a8a9732b7c7fd5a85c1c22e5cdc48
Ivan Gavryliuk <ivan@isolineltd.com>
2017-01-11 10:00:53 +0000

    convertto-json parameter to increase depth

--- ChangeVersion.ps1
+++ ChangeVersion.ps1
@@ -9,11 +9,11 @@ Write-Host "changing core version..."
 $path = "$PSScriptRoot\src\NetBox\project.json"
 $json = Get-Content $path | ConvertFrom-Json
 $json.version = $Version
-$json | ConvertTo-Json | Set-Content -Path $path
+$json | ConvertTo-Json -Depth 100 | Set-Content -Path $path
 
 Write-Host "changing dependencies versions..."
 $path = "$PSScriptRoot\src\NetBox.Tests\project.json"
 $json = Get-Content $path | ConvertFrom-Json
 $json.dependencies.NetBox = $Version
-$json | ConvertTo-Json | Set-Content -Path $path
+$json | ConvertTo-Json -Depth 100 | Set-Content -Path $path
 
