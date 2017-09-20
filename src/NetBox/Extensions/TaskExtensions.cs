56b896cd5d8fdad871d8b1a71c75a5a3742ccd5c
Ivan Gavryliuk <ivan@isolineltd.com>
2017-09-20 13:10:57 +0100

    add forget extension method

---
+++ src/NetBox/Extensions/TaskExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace NetBox.Extensions
+{
+   public static class TaskExtensions
+   {
+      [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "task")]
+      public static void Forget(this Task task)
+      {
+      }
+
+   }
+}
