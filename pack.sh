9dbe4b5b6cbf05b044fae49bcd360487ca546f7e
aloneguid <aloneguid@outlook.com>
2021-09-22 15:04:32 +0100

    ALL SORTS OF THINGS

--- pack.sh
+++ pack.sh
@@ -10,10 +10,14 @@ if [ -f $ONAME ] ; then
 	echo "removed $ONAME"
 fi
 
+# get all the unique "using" directives, sorted and deduplicated
+find . -type f -name "*.cs" ! -iname "*test*" ! -iname "netbox.cs" -exec cat {} + | grep ^using | sort --reverse | uniq > $ONAME
+
 echo "concatenating all .cs files (excluding unit tests) to $ONAME..."
 find . -type f -name "*.cs" ! -iname "*test*" ! -iname "netbox.cs" -print | while read filename; do
 	echo -e "\n\n// FILE: $filename\n\n"
-	cat "$filename"
-done > $ONAME
+	# cat "$filename"
+	cat "$filename" | grep --invert-match ^using
+done >> $ONAME
 
 echo "done."
