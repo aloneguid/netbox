2a57432ac39d58be2ea28e405090925e72104706
IG <aloneguid@outlook.com>
2023-04-14 15:33:20 +0100

    warnings and shit

--- src/NetBox/TempFileTest.cs
+++ src/NetBox/TempFileTest.cs
@@ -1,31 +1,25 @@
-﻿namespace NetBox
-{
-   using global::System.IO;
-   using Xunit;
+﻿namespace NetBox {
+    using global::System.IO;
+    using Xunit;
 
-   public class TempFileTest
-   {
-      [Fact]
-      public void NotInUse()
-      {
-         using(var tf = new TempFile())
-         {
+    public class TempFileTest {
+        [Fact]
+        public void NotInUse() {
+            using(var tf = new TempFile()) {
 
-         }
-      }
+            }
+        }
 
-      [Fact]
-      public void InUse()
-      {
-         string path;
+        [Fact]
+        public void InUse() {
+            string path;
 
-         using(var tf = new TempFile())
-         {
-            path = tf;
-            File.WriteAllText(tf, "test");
-         }
+            using(var tf = new TempFile()) {
+                path = tf;
+                File.WriteAllText(tf, "test");
+            }
 
-         Assert.False(File.Exists(path));
-      }
-   }
+            Assert.False(File.Exists(path));
+        }
+    }
 }
